7f56f217cf32140be7cd0d0f93a4d91da819bee3
xzqkut <142806445+xzqkut@users.noreply.github.com>
2023-08-21 16:44:15 +0200

    Add files via upload

---
+++ NameSurname.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perestanovka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Pavel";
+            string surName = "Yakovlev";
+            Console.WriteLine(name+" "+surName);
+            Console.ReadLine();
+            string trueName;
+            trueName = surName + " " + name;
+            Console.WriteLine(trueName);
+        }
+    }
+}
