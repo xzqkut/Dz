5989e5409dea7340b7b36152cec2ea2f43080097
xzqkut <142806445+xzqkut@users.noreply.github.com>
2023-08-21 16:28:19 +0200

    Add files via upload

---
+++ Picture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = 3;
+            int albumPictures = 52;
+            int everythingIsPossible = albumPictures%row;
+            Console.WriteLine(everythingIsPossible);
+        }
+    }
+}
