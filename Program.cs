1b6c971305557e1ac893753ff912cf6f8771b6d4
xzqkut <142806445+xzqkut@users.noreply.github.com>
2023-08-21 15:34:58 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yakovlev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pointsPlayer;
+            string messagesPlayer;
+            char tag = 'A';
+            double weightCharacter;
+            float heightCharacter;
+            decimal powerCharacter;
+            bool isOpen;
+            byte dataArray;
+            long bigData;
+            short smallNumbers;
+
+
+            
+
+        }
+    }
+}
