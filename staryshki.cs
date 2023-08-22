e2f965fa676dd4b56384011e7add3e9e3c7c1a40
xzqkut <142806445+xzqkut@users.noreply.github.com>
2023-08-22 16:47:06 +0200

    Add files via upload

---
+++ staryshki.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfOldLadies;
+            int minutesPerPerson = 10;
+            Console.Write("Введите кол-во старушек: ");
+            numberOfOldLadies=Convert.ToInt32(Console.ReadLine());
+            int totalMinutes = numberOfOldLadies * minutesPerPerson;
+            int hoursInLine = totalMinutes / 60;
+            int minutesInLine = totalMinutes % 60;
+            Console.WriteLine($"Вы должны отстоять в очереди {hoursInLine} часа и {minutesInLine} минут.");
+        }
+    }
+}
