0fa513d6b4abd7af6bbdf743e6489378a103a28f
xzqkut <142806445+xzqkut@users.noreply.github.com>
2023-08-23 13:22:25 +0200

    Add files via upload

--- staryshki.cs
+++ staryshki.cs
@@ -12,14 +12,14 @@ namespace ConsoleApp4
         {
             int numberOfOldLadies;
             int minutesPerPerson = 10;
-            int timeConverter = 60;
+            int minutesInAnHour = 60;
 
             Console.Write("Введите кол-во старушек: ");
             numberOfOldLadies=Convert.ToInt32(Console.ReadLine());
 
             int totalMinutes = numberOfOldLadies * minutesPerPerson;
-            int hoursInLine = totalMinutes / timeConverter;
-            int minutesInLine = totalMinutes % timeConverter;
+            int hoursInLine = totalMinutes / minutesInAnHour;
+            int minutesInLine = totalMinutes % minutesInAnHour;
 
             Console.WriteLine($"Вы должны отстоять в очереди {hoursInLine} часа и {minutesInLine} минут.");
         }
