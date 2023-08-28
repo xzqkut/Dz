9f13eb3fa75d4d64f8fe9f9dfb8dd8b08d21b946
xzqkut <142806445+xzqkut@users.noreply.github.com>
2023-08-28 20:18:33 +0200

    Add files via upload

---
+++ DayOfBorn.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string exitUser = "Exit";
+            int age;
+
+            Console.WriteLine("Введите ваш возраст:");
+            age = Convert.ToInt32(Console.ReadLine());
+ 
+            while (age-- > 0)
+            {
+                Console.WriteLine("С Днем Рождения!");
+            }
+
+            Console.WriteLine("Введите Exit для выхода");
+            exitUser = Convert.ToString(Console.ReadLine());
+            
+            while(exitUser == "Exit")
+            {
+                Console.WriteLine("Досвидания!");
+                break;
+            }
+        }  
+    }
+}
