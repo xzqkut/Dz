433ceba4bd7859a8cbc76b3f80b0b6f90c231a8a
xzqkut <142806445+xzqkut@users.noreply.github.com>
2023-08-31 15:26:38 +0200

    Add files via upload

---
+++ Command.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            string exitCommand = "Exit";
+
+            Console.WriteLine("Программа будет работать пока не введете Exit");
+            userInput = string.Empty;
+
+            while (Console.ReadLine() != exitCommand)
+            {
+                Console.WriteLine("Программа работает!");
+            }
+            Console.WriteLine("Программа завершена!");
+        }
+       
+    }
+}
