237bff1b24b420dc3a42934a27df02a618ca3eef
xzqkut <142806445+xzqkut@users.noreply.github.com>
2023-09-11 19:20:21 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,37 +1,118 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
+using System.Configuration;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Randomsg
+namespace ConsoleCommand
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
+            const string CommandMenu1 = "SetName";
+            const string CommandMenu2 = "SetPassword";
+            const string CommandMenu3 = "WriteName";
+            const string CommandMenu4 = "ConsoleColor";
+            const string CommandMenu5 = "Exit";
 
-            int minValue = 0;
-            int maxValue = 101;
-            int number = random.Next(minValue, maxValue);
-            int divider1 = 3;
-            int divider2 = 5;
-            int sum = 0;
-
-            Console.WriteLine(number);
-            Console.ReadKey();
-
-            for (int i = 0; i <= number; i++)
+            string enteredСommand = " ";
+            string name = " ";
+            string password = " ";
+            string userInput;
+            bool isRunning = true;
+            while (isRunning)
             {
-                if (i % divider1 == 0 || i % divider2 == 0)
+                Console.Clear();
+                Console.WriteLine("МЕНЮ");
+                Console.WriteLine("////////////////////////////");
+                Console.WriteLine(CommandMenu1 + "-Установить имя");
+                Console.WriteLine(CommandMenu2 + "-Установить пароль");
+                Console.WriteLine(CommandMenu3 + "-Вывести имя.");
+                Console.WriteLine(CommandMenu4 + "-Цвет консоли");
+                Console.WriteLine(CommandMenu5 + "-Выход");
+                Console.WriteLine("//////////////////////////\n");
+                Console.WriteLine("Введите команду из пункта меню");
+                enteredСommand = Console.ReadLine();
+
+                switch (enteredСommand)
                 {
-                    sum += i;
-                    Console.WriteLine(i + "");
+                    case CommandMenu1:
+                        Console.Clear();
+                        Console.WriteLine(CommandMenu1);
+                        Console.Write("Введите имя:");
+                        name = Console.ReadLine();
+                        Console.WriteLine($"Здравствуйте {name}!Для продолжения нажмите Enter.");
+                        Console.ReadKey();
+                        break;
+
+                    case CommandMenu2:
+                        Console.Clear();
+                        Console.WriteLine(CommandMenu2);
+                        Console.WriteLine("Установите пароль:");
+                        password = Console.ReadLine();
+                        Console.WriteLine("Пароль установлен! Для продолжения нажмите Enter");
+                        Console.ReadKey();
+                        break;
+
+                    case CommandMenu3:
+                        Console.WriteLine(CommandMenu3);
+                        Console.WriteLine("Введите пароль чтобы вывести имя:");
+                        userInput = Console.ReadLine();
+
+                        if (userInput != password)
+                        {
+                            Console.WriteLine("Неверный пароль.Доступ закрыт!");
+                            Console.ReadKey();
+                        }
+                        else if (password == userInput)
+                        {
+                            Console.WriteLine($"Приветствую вас {name}");
+                            Console.ReadKey();
+                        }
+                        break;
+
+                    case CommandMenu4:
+                        Console.WriteLine("Выбирите цвет: \n 1) Red \n 2) Green \n 3) Blue \n ");
+                        int color = Int32.Parse(Console.ReadLine());
+                        switch (color)
+                        {
+                            case 1:
+                                Console.BackgroundColor = ConsoleColor.Red;
+                                Console.Clear();
+                                break;
+
+                            case 2:
+                                Console.BackgroundColor = ConsoleColor.Green;
+                                Console.Clear();
+                                break;
+
+                            case 3:
+                                Console.BackgroundColor = ConsoleColor.Blue;
+                                Console.Clear();
+                                Console.WriteLine("Цвет не найден.");
+                                break;
+
+                            default:
+                                Console.WriteLine("Неверный выбор.");
+                                break;   
+                        }
+                        break;
+
+                    case CommandMenu5:
+                        isRunning = false;
+                        Console.WriteLine("Пока.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Неверный ввод");
+                        break;
+
+
                 }
             }
-            Console.WriteLine(sum);
         }
     }
 }
