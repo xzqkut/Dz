61a757e55f8b93ff8be32face69e0450aeb9871a
xzqkut <142806445+xzqkut@users.noreply.github.com>
2023-09-18 14:27:25 +0200

    Add files via upload

---
+++ Rate2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minNumber = 1;
+            int maxNumber = 1000;
+            int result = 1;
+            int basicRate = 2;
+            int rate=0;
+
+            Random random = new Random();
+            int number= random.Next(minNumber, maxNumber);
+
+            while (result < number)
+            {
+                rate++;
+                result*=basicRate;
+            }
+            Console.WriteLine("Случайное число: " + number);
+            Console.WriteLine("Нужная степень двойки: " + rate);
+            Console.WriteLine("Число два в найденной степени: " + result);
+
+        }
+    }
+}
