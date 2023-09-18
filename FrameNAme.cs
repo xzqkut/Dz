de8e524eea28250f52818ab8939058b290462bb6
xzqkut <142806445+xzqkut@users.noreply.github.com>
2023-09-18 15:02:31 +0200

    Add files via upload

---
+++ FrameNAme.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string frameName = "";
+
+            Console.WriteLine("Вывести имя в прямоугольнике с символами");
+            Console.Write("Введите имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Имя {name}, длина имени: {name.Length}");
+
+            Console.WriteLine("Введите символ: ");
+            string symbol= Console.ReadLine();
+            int additionToLength=4;
+            string emptySpaceForSecondLine =" ";
+            Console.WriteLine($"Cимвол: {symbol}");
+
+            Console.WriteLine("Обработка имени:");
+            for (int i = 1; i<=(name.Length + additionToLength); i+=1)
+            {
+                frameName += symbol;
+            }
+
+            string middleLine = frameName[0] + emptySpaceForSecondLine + name + emptySpaceForSecondLine + frameName[frameName.Length - 1];
+            Console.WriteLine(frameName);
+            Console.WriteLine(middleLine);
+            Console.WriteLine(frameName);
+        }
+    }
+}
