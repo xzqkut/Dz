b148a4be669ae4afde6186e40e236361dd36fec5
xzqkut <142806445+xzqkut@users.noreply.github.com>
2023-10-10 15:39:38 +0200

    Add files via upload

---
+++ massiv1010.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberLine = 10;
+            int numberColumn = 10;
+            int[,] array = new int[numberLine, numberColumn];
+            int maxNumber = int.MinValue;
+            int FindingNumberColumns = numberColumn;
+            int FindingNumberLine = numberLine;
+            int changeMark = 0;
+
+            Random random = new Random();
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    int minimumElementArray = 10;
+                    int maximumElementArray = 100;
+                    array[i, j] = random.Next(minimumElementArray, maximumElementArray);
+                    Console.Write(array[i, j] + " ");
+
+                    if (maxNumber < array[i, j])
+                    {
+                        maxNumber = array[i, j];
+                        FindingNumberColumns = i;
+                        FindingNumberLine = j;
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Наибольший элемент матрицы: {maxNumber},число находится под рядом {FindingNumberColumns + 1}, в столбце {FindingNumberLine + 1}");
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] == maxNumber)
+                    {
+                        array[i, j] = changeMark;
+                    }
+
+                    Console.Write(array[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
