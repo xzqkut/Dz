1494e82d6c0b1fe29f0f33199a279d282fdc11b9
xzqkut <142806445+xzqkut@users.noreply.github.com>
2023-10-16 17:04:05 +0200

    Add files via upload

---
+++ massiv1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberLine = 10;
+            int numberColumn = 10;
+            int[,] array = new int[numberLine, numberColumn];
+            int maxNumber = int.MinValue;
+            int markChange = 0;
+            int minimumElementArray = 10;
+            int maximumElementArray = 100;
+
+            Random random = new Random();
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(minimumElementArray, maximumElementArray);
+                    Console.Write(array[i, j] + " ");
+
+                    if (maxNumber < array[i, j])
+                    {
+                        maxNumber = array[i, j];
+                        FindingNumberColumns = i;
+                        FindingNumberLine = j;
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Наибольший элемент матрицы: {maxNumber}");
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] == maxNumber)
+                    {
+                        array[i, j] = markChange;
+                    }
+
+                    Console.Write(array[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
