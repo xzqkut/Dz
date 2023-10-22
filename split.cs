337b3d8fb8e85babbb8bda00ef3977a9b1631a0d
xzqkut <142806445+xzqkut@users.noreply.github.com>
2023-10-22 13:36:20 +0200

    Add files via upload

---
+++ split.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringMassiv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Дана строка с текстом, используя метод строки Split получить массив слов";
+
+            string[] words = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string s in words)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
