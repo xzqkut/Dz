56a328588c8e1fe61c575e997c0bbf15f94c08fd
xzqkut <142806445+xzqkut@users.noreply.github.com>
2023-10-22 13:07:11 +0200

    Add files via upload

--- massiv5.cs
+++ massiv5.cs
@@ -14,37 +14,36 @@ namespace massiv5
             Random rand = new Random();
 
             int arraySize = 10;
-            int[] nums = new int[arraySize];
+            int[] numsArray = new int[arraySize];
             int minValue = 1;
             int maxValue = 11;
 
-
-            for (int i = 0; i < nums.Length ; i++)
+            for (int i = 0; i < numsArray.Length ; i++)
             {
-                for (int j = i; j < nums.Length; j++)
+                for (int j = i; j < numsArray.Length; j++)
                 {
-                    nums[i] = rand.Next(minValue, maxValue);
+                    numsArray[i] = rand.Next(minValue, maxValue);
                 }
             }
 
-            for (int i =0; i < nums.Length-1 ;i++)
+            for (int i =0; i < numsArray.Length-1 ;i++)
             {
-                for(int j = i+1; j < nums.Length; j++)
+                for(int j = i+1; j < numsArray.Length; j++)
                 {
-                    if (nums[i] > nums[j])
+                    if (numsArray[i] > numsArray[j])
                     {
-                        int temp = nums[i];
-
-                        nums[i] = nums[j];
-                        nums[j] = temp;
+                        int tempArray = numsArray[i];
+                        numsArray[i] = numsArray[j];
+                        numsArray[j] = tempArray;
                     }
                 }
             }
-            Console.WriteLine("Вывод отсортированного массива: ");
-            for (int i = 0; i < nums.Length; i++)
-            {
-                Console.WriteLine($"{nums[i]}" + " ");
 
+            Console.WriteLine("Вывод отсортированного массива: ");
+
+            for (int i = 0; i < numsArray.Length; i++)
+            {
+                Console.WriteLine($"{numsArray[i]}" + " ");
             }
             Console.WriteLine();
         }
