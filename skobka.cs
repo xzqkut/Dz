ad06eb8f439a162b58d5d25003a06db5417a5b9a
xzqkut <142806445+xzqkut@users.noreply.github.com>
2023-11-13 20:34:21 +0200

    Add files via upload

---
+++ skobka.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skobka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = " ";
+            input = Console.ReadLine();
+
+            int depth = 0; 
+            int maxDepth = 0; 
+
+            foreach (char symbol in input)
+            {
+                if (symbol == '(')
+                {
+                    depth++; 
+                    if (depth > maxDepth)
+                    {
+                        maxDepth = depth;
+                    }
+                }
+
+                else if (symbol == ')')
+                {
+                    if (depth > 0)
+                    {
+                        depth--;
+                    }
+                    else
+                    {
+                        
+                        Console.WriteLine("Некорректное скобочное выражение");
+                        return;
+                    }
+                }
+            }
+
+            if (depth == 0)
+            {
+                Console.WriteLine($"Строка является корректным скобочным выражением, максимальная глубина: {maxDepth}");
+            }
+            else
+            {
+                Console.WriteLine("Некорректное скобочное выражение");
+            }
+        }
+    }
+}
