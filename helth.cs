8df06b7b0a4c13db8b1c1fe47cd333b80b48f285
xzqkut <142806445+xzqkut@users.noreply.github.com>
2024-02-10 15:52:05 +0200

    Add files via upload

---
+++ helth.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthBAR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int percentHealth = 40;
+            string symbolBar = "#";
+            string nameBar = $"Шкала здоровья при {percentHealth}%\n";
+
+            DrawBar(percentHealth, symbolBar, nameBar);
+        }
+
+        static void DrawBar(int percent, string symbol, string name)
+        {
+            int maxValue = int.Parse(Console.ReadLine());
+            int isDivisor = 100;
+            int value = maxValue * percent / isDivisor;
+            string quantitySymbols = "";
+
+            if (percent >= 0 && percent <= isDivisor)
+            {
+                for (int i = 0; i < value; i++)
+                {
+                    quantitySymbols += symbol;
+                }
+
+                Console.Write($"{name} [{quantitySymbols}");
+
+                symbol = " ";
+                quantitySymbols = "";
+
+                DrawSymbol(value, maxValue, quantitySymbols, symbol);
+
+            }
+            else
+            {
+                Console.WriteLine("Исходные данные некорректные");
+            }
+
+            Console.ReadKey();
+        }
+        static void DrawSymbol(int value, int maxValue, string quantitySymbols, string symbol)
+        {
+            for (int i = value; i < maxValue; i++)
+            {
+                quantitySymbols += symbol;
+            }
+
+            Console.Write($"{quantitySymbols}]");
+
+        }
+    }
+}
+
+
