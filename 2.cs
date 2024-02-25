9540a0e25fffd306521a3929bcb576cb69d09692
xzqkut <142806445+xzqkut@users.noreply.github.com>
2024-02-25 19:20:43 +0200

    Add files via upload

---
+++ 2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2shuffle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] array  =  { 1, 2, 3, 4, 5, 6, 7 };
+            ShuffleArray(array);
+        }
+
+        static void ShuffleArray(int[] array)
+        {
+            Random random = new Random();
+
+            int randomValue;
+            int shuffleElement;
+
+                for(int i = array.Length - 1; i >= 0; i--)
+            {
+                randomValue = random.Next(array[i]);
+                shuffleElement = array[randomValue];
+                
+                Console.Write(shuffleElement+ " ");
+            }
+        }
+
+    }
+}
