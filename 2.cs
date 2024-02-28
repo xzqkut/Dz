f1116a66896e3a77e98fb2f4540a782c7b0c5b0a
xzqkut <142806445+xzqkut@users.noreply.github.com>
2024-02-28 13:39:04 +0200

    Add files via upload

--- 2.cs
+++ 2.cs
@@ -3,54 +3,62 @@
     static void Main(string[] args)
     {
         int[] array = { 1, 2, 3, 4, 5, 6, 7 };
-        bool continueShuffling = true;
+        bool isShuffling = true;
         string responsYes = "Да";
         string responsNo = "Нет";
+        string emptySymbol = " ";
 
-        ShuffleTheArray(array);
+        Console.WriteLine("Исходный массив:");
 
-        while (continueShuffling)
+        PrintArray(array,emptySymbol);
+
+        ShuffleArray(array, emptySymbol);
+
+        while (isShuffling)
         {
-            Console.WriteLine("\nХотите еще раз перетасовать массив? (Да/нет)");
+            Console.WriteLine($"\nХотите еще раз перетасовать массив? ({responsYes}/ {responsNo})");
             string response = Console.ReadLine();
 
             if (response == responsYes)
             {
-                ShuffleTheArray(array);
+                PrintArray(array, emptySymbol);
+                ShuffleArray(array,emptySymbol);
             }
             else if (response == responsNo)
             {
-                continueShuffling = false;
+                isShuffling = false;
             }
             else
             {
-                Console.WriteLine("Неверный ответ. Пожалуйста, введите Да или Нет.");
+                Console.WriteLine($"Неверный ответ. Пожалуйста, введите {responsYes} или {responsNo}.");
             }
         }
 
         Console.WriteLine("Программа прекращена.");
     }
 
-    static void ShuffleTheArray(int[] array)
+    static void ShuffleArray(int[] array, string emptySymbol)
     {
         Random random = new Random();
 
         for (int i = array.Length - 1; i > 0; i--)
         {
-            int j = random.Next(i + 1);
+            int randomIndex = random.Next(i + 1);
             int temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
+            array[i] = array[randomIndex];
+            array[randomIndex] = temp;
         }
-        AcceptShuflleArray(array);
+
+        Console.WriteLine("\n Перетасованный массив:");
+            PrintArray(array,emptySymbol);
     }
 
-    static void AcceptShuflleArray(int[]array)
+    static void PrintArray(int[] array,string emptySymbol)
     {
-        Console.WriteLine("\nМассив перетасован:");
         foreach (var item in array)
         {
-            Console.Write(item + " ");
+            Console.Write(item + emptySymbol) ;
         }
+        Console.WriteLine();
     }
 }
