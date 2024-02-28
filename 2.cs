2b1fd30bd6ac33cd2d6ab644065391d2bf595b63
xzqkut <142806445+xzqkut@users.noreply.github.com>
2024-02-28 11:13:47 +0200

    Add files via upload

--- 2.cs
+++ 2.cs
@@ -10,25 +10,51 @@ namespace _2shuffle
     {
         static void Main(string[] args)
         {
-            int [] array  =  { 1, 2, 3, 4, 5, 6, 7 };
+            int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+            bool continueShuffling = true;
+
             ShuffleArray(array);
+
+            while (continueShuffling)
+            {
+                Console.WriteLine("\nХотите еще раз перетасовать массив? (Да/нет)");
+                string response = Console.ReadLine();
+
+                if (response == "Да")
+                {
+                    ShuffleArray(array);
+                }
+                else if (response == "Нет")
+                {
+                    continueShuffling = false;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный ответ. Пожалуйста, введите Да или Нет.");
+                }
+            }
+
+            Console.WriteLine("Программа прекращена.");
         }
 
         static void ShuffleArray(int[] array)
         {
             Random random = new Random();
 
-            int randomValue;
-            int shuffleElement;
-
-                for(int i = array.Length - 1; i >= 0; i--)
+            for (int i = array.Length - 1; i > 0; i--)
             {
-                randomValue = random.Next(array[i]);
-                shuffleElement = array[randomValue];
-                
-                Console.Write(shuffleElement+ " ");
+                int j = random.Next(i + 1);
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+
+            Console.WriteLine("\nМассив перетасован:");
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
             }
         }
-
     }
 }
+
