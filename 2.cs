19a0209236b72b90f7ec168ec002d97cb5bb65d5
xzqkut <142806445+xzqkut@users.noreply.github.com>
2024-02-28 12:37:06 +0200

    Add files via upload

--- 2.cs
+++ 2.cs
@@ -1,60 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _2shuffle
+﻿internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+        bool continueShuffling = true;
+        string responsYes = "Да";
+        string responsNo = "Нет";
+
+        ShuffleTheArray(array);
+
+        while (continueShuffling)
         {
-            int[] array = { 1, 2, 3, 4, 5, 6, 7 };
-            bool continueShuffling = true;
+            Console.WriteLine("\nХотите еще раз перетасовать массив? (Да/нет)");
+            string response = Console.ReadLine();
 
-            ShuffleArray(array);
-
-            while (continueShuffling)
+            if (response == responsYes)
             {
-                Console.WriteLine("\nХотите еще раз перетасовать массив? (Да/нет)");
-                string response = Console.ReadLine();
-
-                if (response == "Да")
-                {
-                    ShuffleArray(array);
-                }
-                else if (response == "Нет")
-                {
-                    continueShuffling = false;
-                }
-                else
-                {
-                    Console.WriteLine("Неверный ответ. Пожалуйста, введите Да или Нет.");
-                }
+                ShuffleTheArray(array);
             }
-
-            Console.WriteLine("Программа прекращена.");
+            else if (response == responsNo)
+            {
+                continueShuffling = false;
+            }
+            else
+            {
+                Console.WriteLine("Неверный ответ. Пожалуйста, введите Да или Нет.");
+            }
         }
 
-        static void ShuffleArray(int[] array)
+        Console.WriteLine("Программа прекращена.");
+    }
+
+    static void ShuffleTheArray(int[] array)
+    {
+        Random random = new Random();
+
+        for (int i = array.Length - 1; i > 0; i--)
         {
-            Random random = new Random();
+            int j = random.Next(i + 1);
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        AcceptShuflleArray(array);
+    }
 
-            for (int i = array.Length - 1; i > 0; i--)
-            {
-                int j = random.Next(i + 1);
-                int temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-            }
-
-            Console.WriteLine("\nМассив перетасован:");
-            foreach (var item in array)
-            {
-                Console.Write(item + " ");
-            }
+    static void AcceptShuflleArray(int[]array)
+    {
+        Console.WriteLine("\nМассив перетасован:");
+        foreach (var item in array)
+        {
+            Console.Write(item + " ");
         }
     }
 }
-
