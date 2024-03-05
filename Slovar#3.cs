61170a5757529432f319f1c7e461d891715cd29a
xzqkut <142806445+xzqkut@users.noreply.github.com>
2024-03-05 18:59:49 +0200

    Add files via upload

---
+++ Slovar#3.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slovar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string CommandUseDictionary = "1";
+            const string CommandExit = "2";
+
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+            dictionary.Add("Собака", " Собака — домашнее животное, одно из наиболее популярных животных - компаньонов.");
+            dictionary.Add("Змея", "Змеи — подотряд класса пресмыкающихся отряда чешуйчатые");
+            dictionary.Add("Человек", "Человек — общественное существо, обладающее разумом и сознанием, субъект общественно-исторической деятельности и культуры.");
+
+            bool isOpen = true;
+            while (isOpen)
+            {
+                Console.WriteLine($"{CommandUseDictionary})Заглянуть в словарь. \n{CommandExit})Завершить. ");
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case CommandUseDictionary:
+                        SearchWord(dictionary);
+                        break;
+
+                    case CommandExit:
+                        isOpen = false;
+                        break;
+
+                }
+            }
+        }
+
+        static void SearchWord(Dictionary<string,string> dictionary)
+        {
+            Console.WriteLine($"Здравствуйте! О чем бы вам хотелось узнать?\nВыбирете и напишите слово из списка>>\n");
+
+            foreach(string word in dictionary.Keys)
+            {
+                Console.WriteLine(word);
+            }
+
+            string userInput= Console.ReadLine();
+
+            if (dictionary.ContainsKey(userInput))
+            {
+                Console.WriteLine(dictionary[userInput]);
+            }
+            else
+            {
+                Console.WriteLine("Такого слова еще нет.");
+            }
+        }
+
+
+    }
+}
