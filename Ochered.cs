71fef9e74c54d0290f285ec8da74137338e26e74
xzqkut <142806445+xzqkut@users.noreply.github.com>
2024-03-09 16:08:03 +0200

    Add files via upload

---
+++ Ochered.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> clients = GetClients();
+
+            int cash = 0;
+            int numberClients = 1;
+
+            bool isOpen = true;
+
+            while (isOpen)
+            {
+                int money = clients.Dequeue();
+                cash += money;
+
+                Console.WriteLine($"{numberClients++} клиент сделал покупки на сумму {money} рублей.");
+                Console.WriteLine($"В кассе - {cash} рублей.");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        static Queue<int> GetClients()
+        {
+            Queue<int> clients = new Queue<int>();
+            Random rand = new Random();
+            int maxValue = 2000;
+
+            Console.WriteLine("Введите количество клиентов в очереди: ");
+            string userInput = Console.ReadLine();
+
+            if (int.TryParse(userInput, out int number))
+            {
+                for (int i = 0; i < number; i++)
+                {
+                    clients.Enqueue(rand.Next(maxValue));
+                }
+            }
+
+            return clients;
+        }
+    }
+}
