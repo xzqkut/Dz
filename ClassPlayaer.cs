097c568e5104a20194c30a5465984a792e256674
xzqkut <142806445+xzqkut@users.noreply.github.com>
2024-04-09 12:23:01 +0200

    Add files via upload

---
+++ ClassPlayaer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPlayer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+
+            player.ShowInfo();
+        }
+    }
+class Player
+    {
+        private int _health;
+        private string _name;
+        private int _damage;
+
+        public Player(int health, int damage, string name)
+        {
+            if (damage < 0)
+            {
+                damage = 5;
+            }
+            else
+            {
+                _damage = damage;
+            }
+
+            _health = health;
+            _name = name;
+            _damage = damage;
+        }
+
+        public Player()
+        {
+            _health = 100;
+            _name = "kekwiger";
+            _damage = 10;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Игрок имеет {_health}  здоровья, {_damage} урона и его зовут {_name}") ;
+        }
+    }
+
+
+}
