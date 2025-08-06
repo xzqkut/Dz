7609d554a7e5a8deb9c841edbad59aea2143c53d
xzqkut <142806445+xzqkut@users.noreply.github.com>
2025-08-06 16:05:41 +0200

    Add files via upload

--- Card.cs
+++ Card.cs
@@ -10,9 +10,9 @@ namespace ConsoleApp18
             Croupier croupier = new Croupier();
             Console.WriteLine("Сколько карт выдать игроку?");
 
-            if(int.TryParse(Console.ReadLine(),out int cardCount))
+            if (int.TryParse(Console.ReadLine(), out int cardCount))
             {
-                croupier.GiveCardsToPlayer(cardCount);
+                croupier.DealCards(cardCount);
                 croupier.ShowPlayerCards();
             }
             else
@@ -21,28 +21,28 @@ namespace ConsoleApp18
             }
         }
     }
-        
+
     class Croupier
     {
-        private Deck _deck;
-        private Player _player;
+        private Deck deck;
+        private Player player;
 
         public Croupier()
         {
-            _deck = new Deck();
-            _player = new Player();
+            deck = new Deck();
+            player = new Player();
         }
 
         public void ShowPlayerCards()
         {
             Console.WriteLine("\nКарты игрока:");
-            _player.ShowCards();
+            player.ShowCards();
         }
 
-        public void GiveCardsToPlayer(int count)
+        public void DealCards(int count)
         {
-            List<Card>cards = _deck.DrawCards(count);
-            _player.TakeCards(cards);
+            List<Card> cards = deck.DrawCards(count);
+            player.TakeCards(cards);
 
         }
 
@@ -52,8 +52,8 @@ namespace ConsoleApp18
 
     class Deck
     {
-        private List<Card> _cards=new List<Card>();
-        private Random _random=new Random();
+        private List<Card> cards = new List<Card>();
+        private Random random = new Random();
 
         public Deck()
         {
@@ -64,7 +64,7 @@ namespace ConsoleApp18
             {
                 foreach (string value in values)
                 {
-                    _cards.Add(new Card(suit, value));
+                    cards.Add(new Card(suit, value));
                 }
             }
 
@@ -73,45 +73,48 @@ namespace ConsoleApp18
 
         public void Shuffle()
         {
-            int n = _cards.Count;
-            for (int i = n - 1; i > 0; i--)
+            int totalCards = cards.Count;
+            for (int i = totalCards - 1; i > 0; i--)
             {
-                int j = _random.Next(i + 1);
-                (_cards[i], _cards[j]) = (_cards[j], _cards[i]);
+                int randomIndex = random.Next(i + 1);
+                (cards[i], cards[randomIndex]) = (cards[randomIndex], cards[i]);
             }
         }
 
-        public List<Card>DrawCards(int count)
+        public List<Card> DrawCards(int count)
         {
             List<Card> hand = new List<Card>();
-            
-            for (int i = 0; i < count && _cards.Count > 0; i++)
+
+            for (int i = 0; i < count; i++)
             {
-                hand.Add(_cards[0]);
-                _cards.RemoveAt(0);
+                if(cards.Count==0)
+                    break;
+
+                hand.Add(cards[0]);
+                cards.RemoveAt(0);
             }
             return hand;
-        }   
+        }
     }
 
     class Player
     {
-         List<Card>_hand = new List<Card>();
-        
+        List<Card> _cards = new List<Card>();
+
         public void TakeCards(List<Card> cards)
         {
-            _hand.AddRange(cards);
+            _cards.AddRange(cards);
         }
 
         public void ShowCards()
         {
-            if (_hand.Count == 0)
+            if (_cards.Count == 0)
             {
                 Console.WriteLine("Игрок не имеет карт");
             }
             else
             {
-                foreach(Card card in _hand)
+                foreach (Card card in _cards)
                 {
                     Console.WriteLine(card);
                 }
@@ -122,18 +125,18 @@ namespace ConsoleApp18
 
     class Card
     {
-       string _suit {  get; set; }
-        string _value { get; set; }
-
         public Card(string suit, string value)
         {
-            _suit = suit;
-            _value = value;
+            Suit = suit;
+            Value = value;
         }
+
+        public string Suit { get; private set}
+        public string Value { get; private set}
 
         public override string ToString()
         {
-            return $"{_value} { _suit}";
+            return $"{Value} {Suit}";
         }
     }
 }
