fc86a44e3fde1d888a3f658834c78dbad029020e
xzqkut <142806445+xzqkut@users.noreply.github.com>
2025-09-21 19:30:14 +0200

    Add files via upload

--- Library.cs
+++ Library.cs
@@ -8,7 +8,6 @@ namespace Windows
     {
         static void Main(string[] args)
         {
-
             Library library = new Library();
             library.Run();
         }
@@ -16,9 +15,9 @@ namespace Windows
 
     class Book
     {
-        public Book(string nameBook, string author, int years)
+        public Book(string name, string author, int years)
         {
-            NameBook = nameBook;
+            NameBook = name;
             Author = author;
             Years = years;
         }
@@ -31,111 +30,16 @@ namespace Windows
     class Library
     {
         private bool _isOpen = true;
-        private List<Book> Books = new List<Book>();
+        private List<Book> _books = new List<Book>();
 
         public Library()
         {
-            Books.Add(new Book("Капитанская дочка", "А.С Пушкин", 1836));
-            Books.Add(new Book("Унесенные Ветром", "Маргарет Митчелл", 1936));
-            Books.Add(new Book("Скотный Двор", "Джордж Оруэлл", 1945));
-            Books.Add(new Book("Макбет", "Уильям Шекспир", 1623));
-            Books.Add(new Book("12 Стульев", "Илья Ильф и Евгений Петров", 1927));
-            Books.Add(new Book("Анна Каренина", "Л.Н Толстой", 1836));
-        }
-
-        private void ShowAllBook()
-        {
-            Console.WriteLine("СПИСОК ВСЕХ КНИГ В НАЛИЧИИ\n\n");
-
-            PrintBooks();
-        }
-
-        private void AddBook()
-        {
-            Console.WriteLine("Для добавления книги в библиотеку следуйте инструкции");
-
-            Console.WriteLine("Впишите +Название+");
-            string bookName = Console.ReadLine();
-
-            Console.WriteLine("Впишите +Автора книги+");
-            string authorName = Console.ReadLine();
-
-            Console.WriteLine("Укажите +Год издания+");
-
-            if (int.TryParse(Console.ReadLine(), out int yearsBook) && yearsBook > 0)
-            {
-                Books.Add(new Book(bookName, authorName, yearsBook));
-            }
-            else
-            {
-                Console.WriteLine("Некоректный ввод");
-            }
-        }
-
-        private void SearchByName()
-        {
-            Console.WriteLine("Введите название интересующей книги:");
-
-            string name = Console.ReadLine();
-
-            foreach (Book book in Books)
-            {
-                if (book.NameBook.Equals(name))
-                {
-                    Console.WriteLine($"Книга с таким названием есть.\n{name}");
-                }
-                else
-                {
-                    Console.WriteLine("Книга с таким названием отсутствует");
-                }
-            }
-        }
-
-        private void SearchByAuthor()
-        {
-            Console.WriteLine("#Поиск книги по автору#\nВведите имя автора");
-
-            string author = Console.ReadLine();
-
-            foreach (Book book in Books)
-            {
-                if (book.Author.Equals(author))
-                {
-                    author = book.Author;
-
-                    Console.WriteLine($"{author} его книги {book.NameBook}");
-                }
-            }
-        }
-
-        private void PutBookBack()
-        {
-            PrintBooks();
-
-            Console.WriteLine(new string('=', 100));
-
-            int value = Convert.ToInt32(Console.ReadLine()) - 1;
-
-            if (value >= 0 && value < Books.Count)
-            {
-                var removedBook = Books[value];
-                Books.RemoveAt(value);
-                Console.WriteLine($"Книга {removedBook.NameBook} была удалена");
-            }
-            else
-            {
-                Console.WriteLine("Некорректный номер.");
-            }
-        }
-
-        public void PrintBooks()
-        {
-            for (int i = 0; i < Books.Count; i++)
-            {
-                var books = Books[i];
-                Console.WriteLine($"№{i + 1}##Название книги:{books.NameBook}##Автор:{books.Author}##Год издания:{books.Years} ");
-                Console.WriteLine();
-            }
+            _books.Add(new Book("Капитанская дочка", "А.С Пушкин", 1836));
+            _books.Add(new Book("Унесенные Ветром", "Маргарет Митчелл", 1936));
+            _books.Add(new Book("Скотный Двор", "Джордж Оруэлл", 1945));
+            _books.Add(new Book("Макбет", "Уильям Шекспир", 1623));
+            _books.Add(new Book("12 Стульев", "Илья Ильф и Евгений Петров", 1927));
+            _books.Add(new Book("Анна Каренина", "Л.Н Толстой", 1836));
         }
 
         public void Run()
@@ -186,6 +90,101 @@ namespace Windows
                         Console.WriteLine("Нет такого действия");
                         break;
                 }
+            }
+        }
+
+        private void ShowAllBook()
+        {
+            Console.WriteLine("СПИСОК ВСЕХ КНИГ В НАЛИЧИИ\n\n");
+
+            PrintBooks();
+        }
+
+        private void AddBook()
+        {
+            Console.WriteLine("Для добавления книги в библиотеку следуйте инструкции");
+
+            Console.WriteLine("Впишите +Название+");
+            string bookName = Console.ReadLine();
+
+            Console.WriteLine("Впишите +Автора книги+");
+            string authorName = Console.ReadLine();
+
+            Console.WriteLine("Укажите +Год издания+");
+
+            if (int.TryParse(Console.ReadLine(), out int yearsBook) && yearsBook > 0)
+            {
+                _books.Add(new Book(bookName, authorName, yearsBook));
+            }
+            else
+            {
+                Console.WriteLine("Некоректный ввод");
+            }
+        }
+
+        private void SearchByName()
+        {
+            Console.WriteLine("Введите название интересующей книги:");
+
+            string name = Console.ReadLine();
+            
+
+            foreach (Book book in _books)
+            {
+                if (book.NameBook.Equals(name))
+                {
+                    Console.WriteLine($"По вашему запросу мы нашли:\n{name},ее автор:{book.Author} , год издания:{book.Years}\n");
+                }
+            }
+        }
+
+        private void SearchByAuthor()
+        {
+            Console.WriteLine("#Поиск книги по автору#\nВведите имя автора");
+
+            string author = Console.ReadLine();
+
+            foreach (Book book in _books)
+            {
+                if (book.Author.Equals(author))
+                {
+                    author = book.Author;
+
+                    Console.WriteLine($"{author} его книги {book.NameBook}");
+                }
+            }
+        }
+
+        private void PutBookBack()
+        {
+            PrintBooks();
+
+            Console.WriteLine(new string('=', 100));
+
+            int number = 0;
+            int index = 1;
+            string inputNumber = Console.ReadLine();
+
+            if(int.TryParse(inputNumber,out number))
+            {
+                int result=number-index;
+                var removedBook = _books[result];
+                _books.RemoveAt(result);
+                Console.WriteLine($"Книга {removedBook.NameBook} была удалена");
+            }
+            else
+            {
+                Console.WriteLine("Не получилось попробуйте еще раз.\n");
+            }
+        }
+
+        public void PrintBooks()
+        {
+            for (int i = 0; i < _books.Count; i++)
+            {
+                var books = _books[i];
+                Console.WriteLine($"№{i + 1}##Название книги:{books.NameBook}##Автор:{books.Author}##Год издания:{books.Years} ");
+                Console.WriteLine();
             }
         }
     }
